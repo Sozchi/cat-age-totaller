5825a6f80332872a08013c609bd080580d9e0199
victoria ukaonu <73253494+Sozchi@users.noreply.github.com>
2021-06-02 22:30:46 +0100

    finished num 5 and 8

--- src/AgeCounter.js
+++ src/AgeCounter.js
@@ -1,38 +1,22 @@
 import React, { useState } from "react";
 
 const AgeCounter = (props) => {
-  const [age, setAge] = useState("");
-  const [newAge, setNewAge] = useState("");
-  
-  const addAge = () =>
-  {
-    const newCatAge = [...cat, newAge];
-    setAge( newCatAge );
+  const [age, setAge] = useState(0);
+  const isYoungOrOld = age >= 9 ? "old" : "young";
+
+  function ageHandler(change) {
+    setAge(age + change);
+    props.ageTotalHandler(change);
   }
 
-  const addLife = () => {
-    setAge(age - 1);
-  };
   return (
-    <div className="AgeCounter">
+    <div className={`AgeCounter ${isYoungOrOld}`}>
       <h4>{props.cat}</h4>
-      <h3>{0}</h3>
+      <h3>{age}</h3>
 
-      <button onClick={addAge}>Add Year</button>
-      <button onClick={addLife}>New Life</button>
+      <button onClick={() => ageHandler(1)}>Add Year</button>
+      <button onClick={() => ageHandler(-age)}>New Life</button>
     </div>
   );
-}
+};
 export default AgeCounter;
-    
-// 4. Pass each name as a prop to the AgeCounter component and render the name above the current 
-// age in a h4 tag.
-
-    
-    
-    
-// 1. Create a AgeCounter component which uses useState to store the current age and has two buttons -
-// one to add a year( increment state by 1 ) and one to add a new life( set state back to 0 ).
-// Create these functions and use the onClick event on the buttons to use them.Display the current
-// age within the AgeCounter component in a h3 tag.Use a className of ".AgeCounter" for the component
-// ( it should be a div with a h3 tag and two buttons inside, the CSS is already provided in styles.css).
--- src/AgeTotaller.js
+++ src/AgeTotaller.js
@@ -1,26 +1,51 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import AgeCounter from "./AgeCounter";
 
-const AgeTotaller = () =>
-{
-  const [cats, setCats] = useState(["Ming", "Fluffy", "Garfield"]);
-  return (
-    <div className="AgeTotaller">
-      <ul className="">
-        {cats.map((cat, index) => (
-          <li>
-            <AgeCounter cat={cat}/>
-          </li>
-        ))}
-      </ul>
+const AgeTotaller = () => {
+  const [cats, setCats] = useState(["Ming", "Fluffy", "Garfield", "Grumpy"]);
+  const [newCat, setNewCat] = useState("");
 
-      <div className="cats"></div>
+  const [total, setTotal] = useState( 0 );
+  
+  function ageTotalHandler(change)
+  {
+    setTotal( total + change );
+  }
+
+  function addNewCat() {
+    if (newCat.length > 3) {
+      const newCats = [...cats, newCat];
+      setCats(newCats);
+      setNewCat("");
+    }
+  }
+
+  return (
+    <div>
+      <input
+        type="text"
+        onChange={(event) => setNewCat(event.target.value)}
+        value={newCat}
+      />
+      <button type="submit" onClick={addNewCat}>
+        add cat
+      </button>
+
+      <div className="AgeTotaller">
+        <div className="cats">
+          {cats.map((cat) => (
+            <AgeCounter cat={cat} ageTotalHandler={ageTotalHandler} />
+          ))}
+        </div>
+        <h1>The total age is:{total}</h1>
+      </div>
     </div>
   );
 };
 
 export default AgeTotaller;
 
-
-// 3. Use useState to create an array of four cat names in AgeTotaller.js( call the variable "cats" ).
-// Use.map to render an AgeCounter component for each name.
+// 8. Create a function which will update the total age when one of the AgeCounter's current age changes.
+// You will need to pass this function down as a prop to AgeCounter and update from there.
+// Think carefully here about these functions, e.g.if a cat is 8 and you click on the new life button
+// to reset the age to 0, the total age should be decreased by 8.
